90342364e1e90faee81ea2d824d61967868aa1a6
Roushan <kumar7roushanjha2005@gmail.com>
2024-09-13 03:34:29 +0530

    Refactor app.js and index.js, add new route for complex feed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cros from "cros";
-import { userCRUD, feedBack } from "./routes/index.js";
-import {compFeed,userCRUD,feedBack} from './routes/index.js';
 import cookieParser from "cookie-parser";
+
+
 const app = express();
 app.use(cookieParser());
 
@@ -13,6 +13,14 @@ app.use(
   }),
 );
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+import { userCRUD, feedBack } from "./routes/index.js";
+import {compFeed,userCRUD,feedBack} from './routes/index.js';
+
+
 app.get("/", (req, res) => {
   res.send("App is working");
 });
@@ -21,8 +29,5 @@ app.use("/api/v1/user", require(userCRUD));
 app.use("/api/v1/feedback", require(feedBack));
 app.use("/api/v1/feed", require(compFeed));
 // basic need to express
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
 
 export { app };
