9ca6fcdae786f738d2ce128aec4a5f0c1ce9c42d
Roushan <kumar7roushanjha2005@gmail.com>
2024-09-13 00:43:33 +0530

    Refactor app.js and index.js files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
+import _conf from './conf/config.js'
 import { app } from "./app.js";
 import { connectDb } from "./db/connectDb.js";
 
 connectDb()
-  .then()
+  .then(
+    app.listen(_conf.port, () => {
+      console.log(`Server is running on port ${_conf.port}`);
+    }
+  )
+  )
   .catch((err) => {
     console.error("Connection failed !!");
+    console.error(err);
+    process.exit(1);
   });
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,16 +1,21 @@
+import _conf from "../conf/config";
+
 class ApiError extends Error {
-    constructor(statusCode,error= [], stack ="", msg="SomeThing went wrong"){
-        super(msg)
-        this.statusCode= statusCode;
-        this.error= error;
+    constructor(statusCode, error = [], msg = "Something went wrong") {
+        super(msg);
+        this.statusCode = statusCode;
+        this.error = error;
         this.message = msg;
         this.data = null;
         this.success = false;
-        if(stack){
-            this.stack = stack;
-        }
-        else{
-            Error.captureStackTrace(this, this.stack)
+
+        // Enable stack trace capture only in development environment
+        if (_conf.env === "development") {
+            Error.captureStackTrace(this, this.constructor);
+        } else {
+            this.stack = null; // Do not include stack trace in production or other environments
         }
     }
 }
+
+export default ApiError;
