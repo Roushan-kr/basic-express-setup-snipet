54b7779cd0d1d34603b7edff6d88e1f82b78359f
Roushan <kumar7roushanjha2005@gmail.com>
2024-09-13 03:35:16 +0530

    Refactor app.js: Remove unused import statements

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-import { userCRUD, feedBack } from "./routes/index.js";
 import {compFeed,userCRUD,feedBack} from './routes/index.js';
 
 
