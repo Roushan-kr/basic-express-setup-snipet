7b5bd2fff59e5506ac01ba5cc394e55b4483fd15
Roushan <kumar7roushanjha2005@gmail.com>
2024-09-13 04:51:32 +0530

    Refactor import statements in complexFed.js and add welcome message route

--- src/routes/complex/complexFed.js
+++ src/routes/complex/complexFed.js
@@ -1,7 +1,9 @@
 import {Router} from 'express'
 import {findFeedbacksByRating , findFeedbacksByDateRange, findFeedbacksWithCriteria} from '../../controllers/feedback/complex.controller.js'
 const complexFed = Router();
-
+complexFed.get('/', (req, res) => {
+    res.send('Welcome to complex feedbacks');
+});
 complexFed.get('/byRating', findFeedbacksByRating);
 complexFed.get('/byDateRange', findFeedbacksByDateRange);
 complexFed.get('/withCriteria', findFeedbacksWithCriteria);
